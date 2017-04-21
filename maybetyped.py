48b3bfddffd22bbd1b5d53ee15a9b6c72f12a9fc
Nic Manoogian <zimmoz3@verizon.net>
2017-04-21 10:48:55 -0400

    Add non-zero exits for failure cases

--- maybetyped.py
+++ maybetyped.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import sys
 import requests
 import argparse
 try:
@@ -17,9 +18,11 @@ def main(args):
     elif r.status_code == 404:
         cprint("NO", "red")
         print("{} is NOT typed".format(args.module))
+        sys.exit(1)
     else:
         cprint("UGH", "yellow")
         print("Hmmm... there was an issue: {}".format(url))
+        sys.exit(1)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Check if DefinitelyTyped has typings for an npm module')

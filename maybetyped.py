19cd36e66a62dacd8e387854e9321877a3b42fd8
Nic Manoogian <zimmoz3@verizon.net>
2017-04-21 10:37:42 -0400

    Add support for termcolor if you have it

--- maybetyped.py
+++ maybetyped.py
@@ -2,15 +2,23 @@
 
 import requests
 import argparse
+try:
+    from termcolor import cprint
+except ImportError:
+    def cprint(text, color):
+        print(text)
 
 def main(args):
     url = "https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/{}".format(args.module)
     r = requests.get(url)
     if r.status_code == 200:
+        cprint("YES", "green")
         print("{} is typed\n{}".format(args.module, url))
     elif r.status_code == 404:
+        cprint("NO", "red")
         print("{} is NOT typed".format(args.module))
     else:
+        cprint("UGH", "yellow")
         print("Hmmm... there was an issue: {}".format(url))
 
 if __name__ == '__main__':
